d778493e10e4afc3782c6141338e189004989304
Andrew Kane <andrew@chartkick.com>
2020-07-09 04:02:37 -0700

    Separate schema

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,24 +7,4 @@ ENV["PGSLICE_ENV"] = "test"
 
 $url = ENV["PGSLICE_URL"] || "postgres:///pgslice_test"
 $conn = PG::Connection.new($url)
-$conn.exec <<-SQL
-SET client_min_messages = warning;
-DROP TABLE IF EXISTS "Posts_intermediate" CASCADE;
-DROP TABLE IF EXISTS "Posts" CASCADE;
-DROP TABLE IF EXISTS "Posts_retired" CASCADE;
-DROP FUNCTION IF EXISTS "Posts_insert_trigger"();
-DROP TABLE IF EXISTS "Users" CASCADE;
-CREATE TABLE "Users" (
-  "Id" SERIAL PRIMARY KEY
-);
-CREATE TABLE "Posts" (
-  "Id" SERIAL PRIMARY KEY,
-  "UserId" INTEGER,
-  "createdAt" timestamp,
-  "createdAtTz" timestamptz,
-  "createdOn" date,
-  CONSTRAINT "foreign_key_1" FOREIGN KEY ("UserId") REFERENCES "Users"("Id")
-);
-CREATE INDEX ON "Posts" ("createdAt");
-INSERT INTO "Posts" ("createdAt", "createdAtTz", "createdOn") SELECT NOW(), NOW(), NOW() FROM generate_series(1, 10000) n;
-SQL
+$conn.exec File.read("test/support/schema.sql")
